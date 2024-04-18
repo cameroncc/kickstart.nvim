d1a5ae7fc6ebee540b8e4fe591efba0239b10311
Cameron Cummings <33526620+cameroncc@users.noreply.github.com>
2024-04-18 14:27:37 -0500

    Remove call to deprecated harpoon `append()` function

--- init.lua
+++ init.lua
@@ -272,7 +272,7 @@ require('lazy').setup({
       local harpoon = require 'harpoon'
       harpoon:setup()
       vim.keymap.set('n', '<leader>a', function()
-        harpoon:list():append()
+        harpoon:list():add()
       end)
       vim.keymap.set('n', '<C-e>', function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
