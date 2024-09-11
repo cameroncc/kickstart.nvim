92c68e28957ee4c0b6898d4e61a8ca3a11899650
Cameron Cummings <git@cameroncummings.cc>
2024-09-10 20:51:00 -0500

    Use prettier formatter for html

--- init.lua
+++ init.lua
@@ -759,6 +759,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
+        html = { 'prettier' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
