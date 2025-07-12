78bcab39af6fb56dbe07ab84af524fbe79207d41
Cameron Cummings <git@cameroncummings.cc>
2025-07-12 12:53:09 -0500

    Tweak configuration for telescope, todo-comments, and italics

--- init.lua
+++ init.lua
@@ -574,6 +574,14 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+
+      -- Show line numbers in Telescope preview windows
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'TelescopePreviewerLoaded',
+        callback = function()
+          vim.wo.number = true
+        end,
+      })
     end,
   },
 
@@ -1001,13 +1009,98 @@ require('lazy').setup({
     name = 'rose-pine',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
+      require('rose-pine').setup {
+        styles = {
+          italic = false,
+        },
+      }
       -- Load the colorscheme here.
       vim.cmd.colorscheme 'rose-pine-moon'
     end,
   },
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      -- Set up options here
+      require('todo-comments').setup {
+        signs = true, -- show icons in the signs column
+        sign_priority = 8, -- sign priority
+        -- keywords recognized as todo comments
+        keywords = {
+          FIX = {
+            icon = ' ', -- icon used for the sign, and in search results
+            color = 'error', -- can be a hex color, or a named color (see below)
+            alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
+            -- signs = false, -- configure signs for some keywords individually
+          },
+          TODO = { icon = ' ', color = 'info' },
+          HACK = { icon = ' ', color = 'warning' },
+          WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+          PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+          NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+          TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+        },
+        gui_style = {
+          fg = 'NONE', -- The gui style to use for the fg highlight group.
+          bg = 'BOLD', -- The gui style to use for the bg highlight group.
+        },
+        merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+        -- highlighting of the line containing the todo comment
+        -- * before: highlights before the keyword (typically comment characters)
+        -- * keyword: highlights of the keyword
+        -- * after: highlights after the keyword (todo text)
+        highlight = {
+          multiline = true, -- enable multine todo comments
+          multiline_pattern = '^.', -- lua pattern to match the next multiline from the start of the matched keyword
+          multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+          before = '', -- "fg" or "bg" or empty
+          keyword = 'wide', -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+          after = 'fg', -- "fg" or "bg" or empty
+          pattern = { [[.*<(KEYWORDS)\s*:]], [[.*<(KEYWORDS)\s*-]] }, -- pattern or table of patterns, used for highlighting (vim regex)
+          comments_only = true, -- uses treesitter to match keywords in comments only
+          max_line_len = 400, -- ignore lines longer than this
+          exclude = {}, -- list of file types to exclude highlighting
+        },
+        -- list of named colors where we try to extract the guifg from the
+        -- list of highlight groups or use the hex color if hl not found as a fallback
+        colors = {
+          error = { 'DiagnosticError', 'ErrorMsg', '#DC2626' },
+          warning = { 'DiagnosticWarn', 'WarningMsg', '#FBBF24' },
+          info = { 'DiagnosticInfo', '#2563EB' },
+          hint = { 'DiagnosticHint', '#10B981' },
+          default = { 'Identifier', '#7C3AED' },
+          test = { 'Identifier', '#FF00FF' },
+        },
+        search = {
+          command = 'rg',
+          args = {
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+          },
+          -- regex that will be used to match keywords.
+          -- don't replace the (KEYWORDS) placeholder
+          pattern = [[\b(KEYWORDS)\s(:|-)]], -- ripgrep regex (match with colon or hyphen)
+          -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+        },
+      }
+
+      -- Configure keymaps
+      vim.keymap.set('n', ']t', function()
+        require('todo-comments').jump_next()
+      end, { desc = 'Next TODO comment' })
+
+      vim.keymap.set('n', '[t', function()
+        require('todo-comments').jump_prev()
+      end, { desc = 'Previous TODO comment' })
+    end,
+  },
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
