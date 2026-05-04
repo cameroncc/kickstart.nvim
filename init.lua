ccb6e45a0d7ea31c348deca5b8cd84324e839816
Cameron Cummings <git@cameroncummings.cc>
2026-05-03 22:47:52 -0500

    User <leader>tt instead of <leader>tz for terminal split

--- init.lua
+++ init.lua
@@ -226,7 +226,7 @@ vim.keymap.set('n', '<leader>td', function()
 end, { desc = 'Toggle inline diagnostics' })
 
 -- Open a terminal in a split using zsh as the shell
-vim.keymap.set('n', '<leader>tz', ':vsplit | terminal zsh<CR>i', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>tt', ':vsplit | terminal zsh<CR>i', { noremap = true, silent = true })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
